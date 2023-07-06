cb758e9d26a378414612432befa57a87289987c5
wiehl-valentina <valenwiehl@gmail.com>
2023-07-06 07:12:27 -0300

    update

--- app/index.php
+++ app/index.php
@@ -11,18 +11,22 @@ $db->connect();
 
 // Crea una nueva instancia de la aplicación Slim
 $app = AppFactory::create();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 $app->setBasePath('/app');
 
 $app->addErrorMiddleware(true, true, true);
 
-$app->add(new CorsMiddleware([
-    "origin" => ["http://localhost:8000"],
-    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    "headers.allow" => ["Authorization", "Content-Type"],
-    "headers.expose" => [],
-    "credentials" => true,
-    "cache" => 0,
-]));
+// $app->add(new CorsMiddleware([
+//     "origin" => ["http://localhost:8000"],
+//     "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     "headers.allow" => ["Authorization", "Content-Type"],
+//     "headers.expose" => [],
+//     "credentials" => true,
+//     "cache" => 0,
+// ]));
 
 require 'routes/GenreRoutes.php';
 require 'routes/PlatformRoutes.php';
